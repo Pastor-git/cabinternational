52d7711ac467d1a887f51ab64519cfd5ecbaca41
Pastor-git <maciej.pastor@gmail.com>
2022-06-19 11:07:03 +0200

    Jan update

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -10,9 +10,9 @@ class Articles extends React.Component {
     super(props);
     this.state = {
       data: [
-        {id: 1, author: 'Jan Kowalski', title: 'Nagłówek 1', intro: 'Ciekawy artykuł 1'},
-        {id: 2, author: 'Jan Kowalski', title: 'Nagłówek 2', intro: 'Ciekawy artykuł 2'},
-        {id: 3, author: 'Jan Kowalski', title: 'Nagłówek 3', intro: 'Ciekawy artykuł 3'},
+        {id: 1, author: 'Jan', title: 'Nagłówek 1', intro: 'Ciekawy artykuł 1'},
+        {id: 2, author: 'Jan', title: 'Nagłówek 2', intro: 'Ciekawy artykuł 2'},
+        {id: 3, author: 'Jan', title: 'Nagłówek 3', intro: 'Ciekawy artykuł 3'},
       ],
     }
   }
